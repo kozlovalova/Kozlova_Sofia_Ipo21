28236a433a3a51a3634034c75d89b0d8cb3ac175
kozlovalova <kozlova114930@gmail.com>
2026-02-17 23:20:21 +0300

    Add files via upload

---
+++ МДК05.01/Индив.пр.формы/Форма1/Форма1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Форма1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void button_Image_Click(object sender, EventArgs e)
+        {
+            // Создаём стандартное окно выбора картинки
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            // Разрешаем выбирать только картинки
+            dialog.Filter = "Картинки|*.jpg;*.png;*.bmp";
+
+            // Показываем окно и проверяем, нажали ли "ОК"
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // Загружаем выбранную картинку и ставим её фоном формы
+                this.BackgroundImage = Image.FromFile(dialog.FileName);
+
+                // Растягиваем картинку на весь экран
+                this.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+        }
+    }
+}
+
