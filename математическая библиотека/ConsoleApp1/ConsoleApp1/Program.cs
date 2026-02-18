0a767525157703333cd3366c3899ea1c0c471b42
kozlovalova <kozlova114930@gmail.com>
2026-02-18 20:49:32 +0300

    Add files via upload

---
+++ математическая библиотека/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyLibrary;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        BasicMath math = new BasicMath();
+
+        Console.WriteLine(math.Add(10, 5));
+        Console.WriteLine(math.Multiply(4, 3));
+        Console.WriteLine(math.Divide(20, 4));
+    }
+}
---
+++ математическая библиотека/ConsoleApp1/MyLibrary/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class BasicMath
+    {
+        public double Add(double a, double b)
+        // Метод сложения
+        // public — доступен извне
+        // double — возвращаемый тип (число с плавающей точкой)
+        // double a, double b — входные параметры
+        {
+            return a + b;
+            // Возвращает сумму двух чисел
+        }
+
+        public double Subtract(double a, double b)
+        // Метод вычитания
+        {
+            return a - b;
+            // Возвращает разность чисел
+        }
+
+        public double Multiply(double a, double b)
+        // Метод умножения
+        {
+            return a * b;
+            // Возвращает произведение чисел
+        }
+
+        public double Divide(double a, double b)
+        // Метод деления
+        {
+            if (b == 0)
+                // Проверка деления на ноль
+                // Если делитель равен 0 — выполнение дальше невозможно
+                throw new Exception("Деление на ноль!");
+            // Генерируется исключение (ошибка)
+
+            return a / b;
+            // Возвращает результат деления
+        }
+    }
+}
