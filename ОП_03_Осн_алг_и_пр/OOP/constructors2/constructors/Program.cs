eb19109e9517f2c33a75bb76ad388bc1156ba738
lova <kozlova114930@gmail.com>
2026-02-19 15:23:57 +0300

    homework

---
+++ ОП_03_Осн_алг_и_пр/OOP/constructors2/constructors/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructors
+{
+    class Student //1 задание
+    {
+        public Student()
+        {
+        }
+    }
+
+    class Child //2 задание
+    {
+        public Child()
+        {
+        }
+    }
+
+    class Car //3 - 4 задание
+    {
+        public int year;
+        public string name;
+        public string color;
+
+        //3 задание
+        public Car(int year)
+        {
+            this.year = year;
+        }
+
+        //4 задание
+        public Car(string name, string color)
+        {
+            this.name = name;
+            this.color = color;
+        }
+    }
+
+    class Product //5 задание
+    {
+        protected string name;
+
+        public Product(string name)
+        {
+            this.name = name;
+        }
+
+        public Product(Product other)
+        {
+            name = other.name;
+        }
+    }
+
+    class Person //6 задание
+    {
+        private int age;
+
+        public Person()
+        {
+            age = 18;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(age);
+        }
+    }
+
+    class Manager //7 задание
+    {
+        private int age;
+        private string name;
+
+        public Manager(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        public Manager(Manager other)
+        {
+            age = other.age;
+            name = other.name;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Child child1 = new Child(); //2 задание
+
+            Car car = new Car(2025); //3 задание
+
+            Car lada = new Car("LADA VESTA", "black"); //4 задание
+            Car bmw = new Car("BMW X5", "white");
+
+            Product p1 = new Product("первый товар"); //5 задание
+            Product p2 = new Product(p1);
+
+            Person person = new Person(); //6 задание
+            person.Print();
+
+            Manager manager1 = new Manager(20, "Дамир"); //7 задание
+            Manager manager2 = new Manager(manager1);
+        }
+    }
+}
+
