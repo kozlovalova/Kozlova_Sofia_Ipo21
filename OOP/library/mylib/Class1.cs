5039438df9aa7a52fcc511870b8e6505ef92d97b
lova <kozlova114930@gmail.com>
2026-02-19 14:46:42 +0300

    Задания по C#

---
+++ OOP/library/mylib/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mylib
+{
+    public class Class1
+    {
+        public class Person
+        {
+            public string name;
+            public Person(string name)
+
+            {
+                this.name = name;
+            }
+            public void Print()
+
+            {
+                Console.WriteLine("Имя: " + name);
+            }
+        }
+
+    }
+}
